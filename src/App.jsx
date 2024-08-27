da04b99c568362e2227c4f5dc131ff088c16cc00
Magdalin princy <princyrajadurai@gmail.com>
2024-08-28 01:43:32 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+import Mainpage from './components/Mainpage';
+
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+      <Mainpage />
+    </div>
+  );
+};
+
+export default App;
